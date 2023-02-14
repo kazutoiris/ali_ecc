905e966f59f520e6d480680bd95579dace9c27d6
kazutoiris <78157415+kazutoiris@users.noreply.github.com>
2023-02-14 15:09:45 +0800

    add test example

--- README.md
+++ README.md
@@ -7,14 +7,14 @@
 1. Generate random `ECC` key-pair to create a new session.
 2. Get the file’s download url without ` invalid X-Device-Id`.
 
-## How to Use
+## How to use
 
 1. Set the `deviceId` in line 7.
 2. Set the `userId` in line 8.
 3. Set the `authorization` in line 34.
 4. (Optional) Set the private key in line 11,12. (Only if you want to use custom key-pair.)
 
-## Some Details
+## Some details
 
 1. `deviceId` could be found in `DevTools` -> `Application` -> `Local Storage` -> `cna`.
 
@@ -32,7 +32,19 @@ There’re some problems need to be solved.
 
 * [ ] How to generate a `deviceId`?
 * [ ] How to generate `authorization` from cookie or local storage?
-* [ ] How often is `nonce` updated?
+* [ ] How often is `nonce` updated? (Could be frozen?)
+
+## Test
+
+You can use `ali_renew_test.py` to test `nonce` update.
+
+If `nonce` is 0, it will create a session automatically.
+
+If `nonce` is larger than 0, it will renew the session.
+
+**Notice:** If it fails, try to increase the interval.
+
+Now it could pass almost 1000 rounds :thumbsup:
 
 ## LICENSE
 
---
+++ ali_renew_test.py
@@ -0,0 +1,76 @@
+import hashlib
+import ecdsa
+import requests
+import random
+import time
+
+appId = "5dde4e1bdf9e4966b387ba58f4b3fdc3"
+deviceId = "e5173011-XXXX-XXXX-XXXX-XXXXf04f3c62"
+userId = "XXXXf36e37XXXX79bdXXXXdeb6203f67"
+# nonce = 0
+
+# private_key = [175, 87, 171, 214, 222, 196, 127, 36, 25, 50, 237, 179, 71, 81, 49, 196,
+#                250, 103, 115, 203, 138, 179, 192, 182, 43, 175, 233, 72, 200, 14, 64, 254]
+# private_key = int.from_bytes(private_key, byteorder='big')
+private_key = random.randint(1, 2**256-1)
+ecc_pri = ecdsa.SigningKey.from_secret_exponent(
+    private_key, curve=ecdsa.SECP256k1)
+ecc_pub = ecc_pri.get_verifying_key()
+public_key = "04"+ecc_pub.to_string().hex()
+
+
+def r(appId: str, deviceId: str, userId: str, nonce: int) -> str:
+    return f"{appId}:{deviceId}:{userId}:{nonce}"
+
+
+def sign(appId, deviceId, userId, nonce) -> str:
+    sign_dat = ecc_pri.sign(r(appId, deviceId, userId, nonce).encode('utf-8'), entropy=None,
+                            hashfunc=hashlib.sha256)
+    return sign_dat.hex()+"01"
+
+
+for nonce in range(1000):
+    print(f"Testing nonce = {nonce}", end=' ')
+    signature = sign(appId, deviceId, userId, nonce)
+    headers = {
+        "authorization": "Bearer BEARERBEARER",
+        "origin": "https://www.aliyundrive.com",
+        "referer": "https://www.aliyundrive.com/",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41",
+        "x-canary": "client=web,app=adrive,version=v3.17.0",
+        "x-device-id": deviceId,
+        "x-signature": signature,
+    }
+
+    if nonce == 0:
+        req = requests.post(
+            "https://api.aliyundrive.com/users/v1/users/device/create_session",
+            json={
+                "deviceName": "Edge浏览器",
+                "modelName": "Windows网页版",
+                "pubKey": public_key,
+            },
+            headers=headers)
+        # print(req.json())
+        # {'result': True, 'success': True, 'code': None, 'message': None}
+        assert req.json()['result']
+    else:
+        req = requests.post(
+            "https://api.aliyundrive.com/users/v1/users/device/renew_session",
+            json={},
+            headers=headers)
+        # print(req.json())
+        # {'result': True, 'success': True, 'code': None, 'message': None}
+        assert req.json()['result']
+
+    req = requests.post("https://api.aliyundrive.com/v2/file/get_download_url",
+                        json={"expire_sec": 14400, "drive_id": "341789",
+                              "file_id": "63dd352b22e34327c0f84277b389eb381XXXXXXX"},
+                        headers=headers)
+    # print(req.json())
+    # {'domain_id': 'bj29', 'drive_id': '341789', 'file_id': '63dd352b22e34327c0f84277b389eb381XXXXXXX', 'revision_id': '63dd352ba95fd7...
+    assert 'url' in req.json()
+
+    print("Passed!")
+
+    time.sleep(0.5)  # 0.5s is the minimum interval between requests
